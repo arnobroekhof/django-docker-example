47d750ca7a2e977d3d64759f8971405f51b408d9
Arno Broekhof <arnobroekhof@gmail.com>
2021-01-07 12:03:55 +0100

    fixed error

--- example/settings.py
+++ example/settings.py
@@ -70,8 +70,6 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-import os
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv("DATABASE_ENGINE", default='django.db.backends.sqlite3'),
